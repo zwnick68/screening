7c413a0700cd5da4ce2bb0a6b7b4a1db80dbba4b
Nicholas Zwiren <zwnick68@gmail.com>
2022-10-06 00:52:39 -0400

    description with video on click is functional, need to highlight text next

--- src/components/List.js
+++ src/components/List.js
@@ -4,20 +4,27 @@ import {useState} from "react"
 import DescriptionVideo from "./DescriptionVideo"
 const List = ({data}) => {
     
-    const [modal,setModal] = useState(true)
-    const [name, description] = [data.name, data.description]
-
+    const [modal,setModal] = useState(false)
+    const [name, description, video] = [data.name, data.description]
+   // console.log(data.video.url)
     const toggleModal = () => {
         setModal(!modal)
-        
     }
-    
+    const Description = () => {
+        return (
+            <>
+            <p>{description}</p>
+            <video loop autoPlay>
+            <source controls onload='this.play()' src={data.video.url} type='video/mp4'></source>
+            </video>
+            </>
+        )
+    }
     return ( 
     <div>
         <li onClick={toggleModal}>{name}</li>
-        
+        { modal ? <Description/> : null}
     </div>
-        
     )
 }
 
