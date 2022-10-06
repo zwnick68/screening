8c1b12572aaf01b4c5c6d8c3571efeacfe62c8b0
Nicholas Zwiren <zwnick68@gmail.com>
2022-10-06 02:41:33 -0400

    readme created

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -3,10 +3,10 @@ import ExerciseFilter from "./ExerciseFilter"
 import List from "./List"
 // created list container at 32 min in
 
-const ListContainer = ({data, setData, allData, setAllData}) => {
+const ListContainer = ({data, setData, allData}) => {
     return ( 
     <div>
-        <ExerciseFilter allData={allData} setAllData={setAllData} data={data} setData={setData}/>
+        <ExerciseFilter allData={allData} data={data} setData={setData}/>
     {
         data.map((e) => {
         return (
